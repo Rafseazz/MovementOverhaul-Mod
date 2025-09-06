2505e0ec7faa835cba11977252f1d9275f299f23
Rafseazz <76096474+Rafseazz@users.noreply.github.com>
2025-09-06 10:46:32 +0800

    stable build

--- MovementOverhaul/obj/Debug/net6.0/MovementOverhaul.AssemblyInfo.cs
+++ MovementOverhaul/obj/Debug/net6.0/MovementOverhaul.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovementOverhaul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9262a71c4109fb7b1f7e88ad8ee7b4e14d7aca2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2224300ba1012bde5f2f2839a20610551a659c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovementOverhaul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovementOverhaul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
